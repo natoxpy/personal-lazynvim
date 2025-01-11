5f4aba49c252c838de41c74e5458ce0ea6028c12
toxpy <natoxpy@gmail.com>
2025-01-11 13:17:57 -0500

    feat: pre-os-termination commit

---
+++ lua/plugins/codesnap.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        "natoxpy/codesnap.nvim",
+        build = "make",
+        opts = {
+            watermark = "",
+            bg_color = "#24273a",
+            bg_editor_color = "#1e2030",
+            bg_x_padding = 50,
+            bg_y_padding = 25,
+        },
+    },
+}
